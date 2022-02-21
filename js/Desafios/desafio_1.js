3a03c8213275071f27cb91eb88a4184b778fcd07
carof88 <carolinaf88@gmail.com>
2022-02-21 20:36:53 -0300

    Creacion Desafio-1

---
+++ js/Desafios/desafio_1.js
@@ -0,0 +1,29 @@
+// Simulador Interactivo
+// Utilizando condicionales, ciclos y funciones
+
+// Calcular IVA (21%) sobre el precio ingresado, 
+// indicando si es mayor o menos el precio final
+
+let price = parseInt(prompt('Ingresar precio:'));
+
+let askprice = (price) => {
+    if ((price >= 1001) && (price <= 5000)) {
+        alert('El precio es mayor a $1000');
+        return price * 1.21;
+    }else if (price >= 5001) {
+        alert('El precio es mayor a $5000, el IVA es mas caro');
+        return price * 1.21;
+    }else {
+        return price * 1.21;
+    }
+}
+
+let result = askprice(price);
+
+console.log(`El precio total con IVA incluido es ${result}`);
+
+
+
+
+
+
---
+++ js/Proyecto_Final/final.js
@@ -0,0 +1,10 @@
+// A que ritmo corrio el km, pedir que ingrese la cantidad de km que corrio
+//Pasar km a millas
+//Pace Calculator
+
+// let kmDay = parseInt(prompt('Ingresar Kilometros corridos:'));
+// let timeDay = prompt('Ingresar cuanto tiempo corriste:')
+
+// let kmConvert = (kmDay) => {
+//     return kmDay 
+// }
