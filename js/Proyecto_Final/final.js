04455b5f711f06cb4a0a4ef126027ce316f56bb1
Caro <caro@192.168.1.6>
2022-03-21 19:40:35 -0300

    Segunda Entrega Proy. Final

--- js/Proyecto_Final/final.js
+++ js/Proyecto_Final/final.js
@@ -1,28 +1,58 @@
 //Calcular ritmo: divido tiempo total / total de km corridos
+const storeElements = localStorage.getItem('elements')
+let array = [];
 
-
-//Nueva interaccion
-let distancia = document.getElementById('dist').value;
-let tiempoTotal = document.getElementById('ttHour').value;
+console.log(localStorage.getItem('elements'));
 
 let getData = function () {
-    const distancia = document.getElementById('dist').value;
     const tiempoTotal = document.getElementById('ttHour').value;
-    console.log(`${distancia} ${tiempoTotal}`);
+    const distancia = document.getElementById('dist').value;
+
+    if (distancia.trim() && tiempoTotal.trim()) {
+        array.push(tiempoTotal);
+        array.push(distancia);
+        document.getElementById('dist').value = '';
+        document.getElementById('ttHour').value = '';
+        mostrarUltimos();
+        localStorage.setItem('elements', JSON.stringify(array))
+    } else {
+        alert('Ingresa un dato')
+    }
 }
 
 
-const array = [tiempoTotal, distancia];
-const resultado = [];
-const dividor = array[1];
+function mostrarUltimos() {
+    const container = document.getElementById('ingresos')
+    container.innerHTML = '';
 
-array.forEach(function(elemento, indice) {
-    resultado[indice] = elemento / dividor;
-});
+    for (const element of array) {
+        console.log(element);
+        const item = document.createElement('li');
+        item.className = "list-item";
+        item.textContent = element;
+        container.appendChild(item);
+    }
+    const resultado = [];
+    const dividor = array[1];
 
-console.log(`Corri a ${resultado[0]} min por km`);
+    array.forEach(function (elemento, indice) {
+        resultado[indice] = elemento / dividor;
+    });
 
-//Evento
+    console.log(`Corri a ${resultado[0]} min por km`);
+}
 
+//Eventos Click
 const button = document.querySelector("#runButton")
-button.addEventListener('click', () => alert("ESTO ES UN EVENTO: Falta completar los campos"))
+button.addEventListener('click', getData);
+
+//En construccion
+// const buttonReset = document.querySelector("#resetButton");
+// buttonReset.addEventListener('click', clearAll);
+
+// function clearAll() {
+    
+// }
+
+
+
